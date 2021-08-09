4dafd85f47c3769583eda15a46c3c11f8e0d8750
Ammad <kh.ammad123@gmail.com>
2021-08-10 00:45:48 +0500

    Authorization component

--- config/initializers/rswag-ui.rb
+++ config/initializers/rswag-ui.rb
@@ -8,7 +8,7 @@ Rswag::Ui.configure do |c|
 
   c.swagger_endpoint '/api-docs/v1/swagger.json', 'API V1 Docs'
 
-  # Add Basic Auth in case your API is private
+  # # Add Basic Auth in case your API is private
   # c.basic_auth_enabled = true
-  # c.basic_auth_credentials 'username', 'password'
+  # c.basic_auth_credentials 'new_wave_assignment', 'scJmc"2uTEtQ'
 end
